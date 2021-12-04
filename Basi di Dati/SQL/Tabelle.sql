510094048f70bc1f5a0cd680d8561f452e4269be
Luigi Penza <91316353+Hex-adecimal@users.noreply.github.com>
2021-12-04 16:50:53 +0100

    Update Tabelle.sql

--- Basi di Dati/SQL/Tabelle.sql
+++ Basi di Dati/SQL/Tabelle.sql
@@ -1,9 +1,36 @@
--- Lunghezza dei nomi, cognomi 35 caratteri
 CREATE TABLE STUDENT(
-	StudentID VARCHAR(9) NOT NULL,
+	StudentID  NOT NULL,
 	FirstName VARCHAR(35) NOT NULL,
 	LastName VARCHAR(35) NOT NULL
 	Email 
 	
 	PRIMARY KEY(Matricola)
 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
