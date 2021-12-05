39b978ec74119875ff8e5946c82bdfbf9fe81aab
MoodSwing159 <93043279+MoodSwing159@users.noreply.github.com>
2021-12-05 12:41:06 +0100

    Update Vincoli.sql

--- Basi di Dati/SQL/Vincoli.sql
+++ Basi di Dati/SQL/Vincoli.sql
@@ -22,7 +22,7 @@ CREATE DOMAIN EMAIL AS VARCHAR(254)
 	
 -- Strong_Password : La password deve essere composta da più di 8 caratteri, almeno una lettera, almeno un numero ed almeno carattere speciale (!"£$%&/()=_:;,.-+*#)
 CREATE DOMAIN PASSWORD_D AS VARCHAR(128)
-	CHECK ( VALUE LIKE '________');
+	CHECK ( VALUE LIKE '________%');
 	
 -- Condizioni da aggiungere al dominio PASSWORD_D in qualche modo: AND VALUE LIKE '%[a-z]%' AND VALUE LIKE '%[0-9]%' AND VALUE LIKE '%[!"£$%&/()=_:;,.-+*#]%'
 
