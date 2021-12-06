6ee103294bd8d2e3f6b2eab1579e4d273b71160b
Luigi Penza <91316353+Hex-adecimal@users.noreply.github.com>
2021-12-06 20:11:36 +0100

    Update Popolazione.sql

--- Basi di Dati/SQL/Popolazione.sql
+++ Basi di Dati/SQL/Popolazione.sql
@@ -1,3 +1,57 @@
-INSERT INTO STUDENT VALUES
-  ('N86004657')
-  ('N86003822')
+-- //------------------------------------------------------- PROFESSOR ----------------------------------------------------------//
+INSERT INTO PROFESSOR(FirstName, LastName, Email, Username, Pw) VALUES
+	('Silvio', 'Barra', 'silvio.barra@unina.it', 'SilvioBarra', 'LaPasswordSegretaDelProfessore!1'),
+  	('Porfirio', 'Tramontana', 'porfirio.tramontana@unina.it', 'PorfirioTramontana', 'LaPasswordSegretaDelProfessore!2'),
+  	('Guglielmo', 'Tamburrini', 'guglielmo.tamburrini@unina.it', 'GuglielmoTamburrini', 'LaPasswordSegretaDelProfessore!3'),
+  	('Fabio', 'Mogaver', 'fabio.mogaver@unina.it', 'FabioMogaver', 'LaPasswordSegretaDelProfessore!4'),
+  	('Eleonora', 'Messina', 'eleonora.messina@unina.it', 'EleonoraMessina', 'LaPasswordSegretaDelProfessore!5'),
+  	('Giovanni', 'Cutolo', 'giovanni.cutolo@unina.it', 'GiovanniCutolo', 'LaPasswordSegretaDelProfessore!6'),
+  	('Francesca', 'Cioffi', 'francesca.cioffi@unina.it', 'FrancescaCioffi', 'LaPasswordSegretaDelProfessore!7'),
+  	('Daniele', 'Castorina', 'daniele.castorina@unina.it', 'DanieleCastorina', 'LaPasswordSegretaDelProfessore!8'),
+  	('Silvia', 'Rossi', 'silvia.rossi@unina.it', 'SilviaRossi', 'LaPasswordSegretaDelProfessore!9'),
+  	('Francesco', 'Isgrò', 'francesc.isgro@unina.it', 'FrancescoIsgro', 'LaPasswordSegretaDelProfessore!10');
+  
+-- //------------------------------------------------------ STUDENT -------------------------------------------------------------// 
+INSERT INTO STUDENT(FirstName, LastName, Email, Username, Pw) VALUES
+  	('Francesco', 'Orlando', 'f.orlando@studenti.unina.it', 'Effeo', 'Giallo1_'),
+  	('Alfredo', 'Laino', 'a.laino@studenti.unina.it', 'pino.pompino', 'Rosso2?'),
+  	('Marco', 'Pastore', 'm.pastore@studenti.unina.it', 'marco_pastazio', 'BluElettrico$'),
+  	('Giorgio', 'Longobardo', 'g.longobardo@studenti.unina.it', 'giovgio', 'RamarroMarron3?');
+	
+-- //------------------------------------------------------ CLASS ---------------------------------------------------------------// 
+INSERT INTO CLASS(Name, Year, CFU, CodP) VALUES
+  	('', '', '', '')
+  
+-- //------------------------------------------------------ TAKE ----------------------------------------------------------------// 
+INSERT INTO TAKE VALUES
+	('', '')
+  
+-- //------------------------------------------------------ OPENANSWER ----------------------------------------------------------// 
+INSERT INTO OPENANSWER VALUES
+	
+
+-- //------------------------------------------------------ CLOSEDANSWER --------------------------------------------------------// 
+INSERT INTO CLOSEDANSWER VALUES
+
+
+-- //------------------------------------------------------ TESTTAKEN -----------------------------------------------------------// 
+INSERT INTO TESTTAKEN VALUES
+
+
+-- //------------------------------------------------------ CLOSEDQUIZ ----------------------------------------------------------// 
+INSERT INTO CLOSEDQUIZ VALUES
+
+
+-- //------------------------------------------------------ OPENQUIZ ------------------------------------------------------------// 
+INSERT INTO OPENQUIZ VALUES
+
+
+-- //------------------------------------------------------ LECTURE -------------------------------------------------------------// 
+INSERT INTO LECTURE VALUES
+
+
+-- //------------------------------------------------------ TEST ---------------------------------------------------------------// 
+INSERT INTO TEST VALUES
+
+
+
