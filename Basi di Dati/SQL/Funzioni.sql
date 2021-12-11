99ce01c6fa2c00d76a4cbcc0296afa6d6cc0bede
Luigi Penza <91316353+Hex-adecimal@users.noreply.github.com>
2021-12-11 20:06:54 +0100

    Create Funzioni.sql

---
+++ Basi di Dati/SQL/Funzioni.sql
@@ -0,0 +1,64 @@
+-- Quando viene inserito un Test, viene automaticamente aggiornato il campo CreationDate;
+-- Quando viene inserita una Class, viene automaticamente aggiornato il campo Year all'anno corrente;
+
+-- Valid_Right_Answer : La risposta di una domanda multipla deve tra quelle possibili (Dominio = {'a', 'b', 'c', 'd'})
+CREATE DOMAIN CLOSED_ANSWER AS CHAR(1)
+	CHECK ( VALUE IN ('a', 'b', 'c', 'd') ); -- Aggiungere il controllo per c e d
+-- Questo vincolo verrà implementato tramite funzione
+
+-- //-------------------------------------------------------------------------//
+-- Quando viene aggiunta una risposta ad una domanda chiusa, si controlla se le soluzioni delle domande a risposta multipla date sono corrette;
+CREATE FUNCTION UCQS_function() RETURNS TRIGGER AS $$
+BEGIN
+	
+	IF EXISTS  (SELECT *
+				FROM CLOSED_ANSWER AS CA, CLOSED_QUIZ AS CQ
+				WHERE CA.CodCQ = CQ.CodCQ
+					AND CA.CodCA = NEW.CodCA
+					AND CA.GivenAnswer = CQ.RightAnswer) THEN
+		
+		UPDATE CLOSED_ANSWER
+		SET Score = (SELECT ScoreIfRight
+					 FROM CLOSED_ANSWER AS CA, CLOSED_QUIZ AS CQ
+					 WHERE CA.CodCQ = CQ.CodCQ
+						AND CA.CodCA = NEW.CodCA);
+		
+	ELSE
+
+		UPDATE CLOSED_ANSWER
+		SET Score = (SELECT ScoreIfWrong
+					 FROM CLOSED_ANSWER AS CA, CLOSED_QUIZ AS CQ
+					 WHERE CA.CodCQ = CQ.CodCQ
+						AND CA.CodCA = NEW.CodCA);
+
+	END IF;
+
+	RETURN NEW;
+END; $$ LANGUAGE PLPGSQL;
+
+CREATE TRIGGER Update_CQ_Score
+AFTER INSERT ON CLOSED_ANSWER
+FOR EACH ROW
+EXECUTE PROCEDURE UCQS_function();
+
+-- //-------------------------------------------------------------------------//
+-- Valid_GivenAnswer: La lunghezza della risposta data NON deve superare MaxLength dell'OpenQuiz associato
+CREATE FUNCTION VGA_function() RETURNS TRIGGER AS $$
+BEGIN
+	IF LENGTH(NEW.GivenAnswer) > (  SELECT MaxLength
+									FROM OPEN_ANSWER AS OA, OPEN_QUIZ AS OQ
+									WHERE OA.CodOQ = OQ.CodOQ
+										AND OA.CodOA = NEW.CodOA ) THEN
+
+			-- errore, fai qualcosa a riguardo
+		RAISE EXCEPTION 'ERRORE! Risposta troppo lunga!';
+
+	END IF;
+
+	RETURN NEW;
+END; $$ LANGUAGE PLPGSQL;
+
+CREATE TRIGGER Valid_GivenAnswer
+AFTER INSERT ON OPEN_ANSWER
+FOR EACH ROW
+EXECUTE PROCEDURE VGA_function();
