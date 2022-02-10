c63d61f5fd36b2c23175566b394bc8a5f32cc9e9
MoodSwing159 <93043279+MoodSwing159@users.noreply.github.com>
2022-02-10 14:41:50 +0100

    Delete Object orientation/Codice sorgente/src/Model directory

--- Object orientation/Codice sorgente/src/Model/Professor.java
+++
@@ -1,7 +0,0 @@
-package Model;
-
-public class Professor extends User{
-	// Attributes 
-	
-	// Methods
-}
--- Object orientation/Codice sorgente/src/Model/Quiz.java
+++
@@ -1,9 +0,0 @@
-package Model;
-
-public class Quiz {
-	// Attributes
-	protected String question;
-	
-	// Methods
-	
-}
