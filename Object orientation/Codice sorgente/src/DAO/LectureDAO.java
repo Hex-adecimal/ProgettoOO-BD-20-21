ab2ef36e1b34729395584e90b43033f3270ce1d9
MoodSwing159 <93043279+MoodSwing159@users.noreply.github.com>
2022-02-10 14:38:44 +0100

    Delete Object orientation/Codice sorgente/src/DAO directory

--- Object orientation/Codice sorgente/src/DAO/LectureDAO.java
+++
@@ -1,9 +0,0 @@
-package DAO;
-
-public interface LectureDAO {
-	public String getTitle(int codLecture);
-	public String getLink(int codLecture);
-	
-	public Void setTitle(int codLecture, String title);
-	public Void setLink(int codLecture, String link);
-}
--- Object orientation/Codice sorgente/src/DAO/QuizDAO.java
+++
@@ -1,7 +0,0 @@
-package DAO;
-
-public interface QuizDAO {
-	public String getQuestion(int codQuiz);
-	
-	public Void setQuestion(int codQuiz, String question);
-}
