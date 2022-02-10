fe1b8facacef0e046cd363c8c104495b63944f39
MoodSwing159 <93043279+MoodSwing159@users.noreply.github.com>
2022-02-10 14:39:56 +0100

    Add files via upload

---
+++ Object orientation/Codice sorgente/src/PostgreImplementationDAO/ClosedQuizPostgre.java
@@ -0,0 +1,103 @@
+package PostgreImplementationDAO;
+
+import DAO.ClosedQuizDAO;
+
+public class ClosedQuizPostgre implements ClosedQuizDAO{
+
+	@Override
+	public String getQuestion(int codQuiz) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void setQuestion(int codQuiz, String question) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getAnswerA(int codClosedQuiz) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getAnswerB(int codClosedQuiz) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getAnswerC(int codClosedQuiz) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getAnswerD(int codClosedQuiz) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getRightAnswer(int codClosedQuiz) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getScoreIfRight(int codClosedQuiz) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getScoreIfWrong(int codClosedQuiz) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void setAnswerA(int codClosedQuiz, String answerA) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void setAnswerB(int codClosedQuiz, String answerB) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void setAnswerC(int codClosedQuiz, String answerC) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void setAnswerD(int codClosedQuiz, String answerD) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void setRightAnswer(int codClosedQuiz, char rightAnswer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void setScoreIfRight(int codClosedQuiz, float scoreIfRight) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void setScoreIfWrong(int codClosedQuiz, float scoreIfWrong) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
---
+++ Object orientation/Codice sorgente/src/PostgreImplementationDAO/OpenQuizPostgre.java
@@ -0,0 +1,55 @@
+package PostgreImplementationDAO;
+
+import DAO.OpenQuizDAO;
+
+public class OpenQuizPostgre implements OpenQuizDAO {
+
+	@Override
+	public String getQuestion(int codQuiz) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void setQuestion(int codQuiz, String question) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public float getMaxScore(int codOpenQuiz) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getMinScore(int codOpenQuiz) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMaxLength(int codOpenQuiz) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Void setMaxScore(int codOpenQuiz, float maxScore) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void setMinScore(int codOpenQuiz, float minScore) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void setMaxLength(int codOpenQuiz, int maxLength) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
