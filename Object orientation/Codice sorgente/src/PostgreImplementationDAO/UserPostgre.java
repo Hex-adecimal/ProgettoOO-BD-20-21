cc36cb9b26227887cdf0728f537084d861ca413e
MoodSwing159 <93043279+MoodSwing159@users.noreply.github.com>
2022-02-10 14:37:19 +0100

    Add files via upload

--- Object orientation/Codice sorgente/src/PostgreImplementationDAO/UserPostgre.java
+++ Object orientation/Codice sorgente/src/PostgreImplementationDAO/UserPostgre.java
@@ -1,14 +1,85 @@
 package PostgreImplementationDAO;
 
+import java.sql.*;
+
 import DAO.UserDAO;
+import Database.QuizDBConnection;
+import Model.Professor;
+import Model.Student;
 import Model.User;
 
 public class UserPostgre implements UserDAO{
-
+	
+	private Connection conn = null;
+	
+	public UserPostgre()
+	{
+		try {
+			conn = QuizDBConnection.getInstance().getConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	@Override
-	public User logUser(String email, String password) {
-		// TODO Auto-generated method stub
-		return null;
+	public User logUser(String userInput, String password) {
+		try
+		{
+			Statement stmt = conn.createStatement();
+			
+			String queryS1 = "SELECT * FROM STUDENT WHERE ";
+			String queryP1 = "SELECT * FROM PROFESSOR WHERE ";
+			String query2;
+			String query3 = " AND Pw = '" + password + "';";
+			String queryStudent;
+			String queryProfessor;
+			
+			if(userInput.contains("@"))
+				query2 = "Email = '" + userInput + "'";
+			else
+				query2 = "Username = '" + userInput + "'";
+			
+			queryStudent = queryS1 + query2 + query3;
+			
+			ResultSet rs = stmt.executeQuery(queryStudent);
+			
+			if(rs.next())
+			{
+				Student userStudent = new Student(rs.getString("StudentID"), 
+												rs.getString("FirstName"), 
+												rs.getString("LastName"),
+												rs.getString("Email"),
+												rs.getString("Username"),
+												rs.getString("Pw"));
+				
+				return userStudent;
+			}
+			else
+			{
+				queryProfessor = queryP1 + query2 + query3;
+				
+				rs = stmt.executeQuery(queryProfessor);
+				
+				if(rs.next())
+				{
+					Professor userProfessor = new Professor(rs.getString("FirstName"), 
+													rs.getString("LastName"),
+													rs.getString("Email"),
+													rs.getString("Username"),
+													rs.getString("Pw"));
+					
+					return userProfessor;
+				}
+				else
+					return null;
+			}
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	@Override
