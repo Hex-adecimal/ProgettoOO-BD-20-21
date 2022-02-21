91f56528a499b4bef032eaa62c1468689540d803
MoodSwing159 <93043279+MoodSwing159@users.noreply.github.com>
2022-02-21 15:34:31 +0100

    Add files via upload
    CHANGES:
    - Added attributes "professor", and "receivingClass", with getters and setters, to implement relationships with other classes in the Model

--- Object orientation/Codice sorgente/src/Model/Lecture.java
+++ Object orientation/Codice sorgente/src/Model/Lecture.java
@@ -5,10 +5,20 @@ public class Lecture {
 	private String title;
 	private String link;
 	
+	private Professor professor;
+	private Class receivingClass;
+	
 	// Methods
 	public String getTitle() { return title; }
 	public void setTitle(String title) { this.title = title; }
 	
 	public String getLink() { return link; }
 	public void setLink(String link) { this.link = link; }
+	
+	
+	public Professor getProfessor() { return professor;	}
+	public void setProfessor(Professor professor) { this.professor = professor;	}
+	
+	public Class getReceivingClass() { return receivingClass; }
+	public void setReceivingClass(Class receivingClass) { this.receivingClass = receivingClass;	}
 }
