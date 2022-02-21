a6007521e8e36635f95c12b22b23d05ea11c7de6
MoodSwing159 <93043279+MoodSwing159@users.noreply.github.com>
2022-02-21 13:27:28 +0100

    Add files via upload
    CHANGES: 
    
    - Added attribute "codP" for future features to be implementable, and modified constructors accordingly;

--- Object orientation/Codice sorgente/src/Model/Professor.java
+++ Object orientation/Codice sorgente/src/Model/Professor.java
@@ -4,16 +4,29 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Professor extends User{
-	// Attributes 
+	// Attributes
+	private String codP;
 	
 	// Methods
-	public Professor(String firstName, String lastName, String email, String username, String password)
+	public Professor(String codP, String firstName, String lastName, String email, String username, String password)
 	{
 		super(firstName, lastName, email, username, password);
+		
+		this.setCodP(codP);
 	}
 	
 	public Professor(ResultSet rs) throws SQLException
 	{
 		super(rs);
+		
+		this.setCodP(rs.getString("codp"));
+	}
+
+	public String getCodP() {
+		return codP;
+	}
+
+	public void setCodP(String codP) {
+		this.codP = codP;
 	}
 }
