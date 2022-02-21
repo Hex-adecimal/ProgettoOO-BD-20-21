8b0f65a38e90e409ff48ea5d344dbac422d2aa53
MoodSwing159 <93043279+MoodSwing159@users.noreply.github.com>
2022-02-21 15:36:43 +0100

    Add files via upload
    CHANGES:
    - Added attributes "testTaken", and "quiz", with getters and setters, to implement relationships with other classes in the Model

--- Object orientation/Codice sorgente/src/Model/ClosedAnswer.java
+++ Object orientation/Codice sorgente/src/Model/ClosedAnswer.java
@@ -5,10 +5,20 @@ public class ClosedAnswer {
 	private char givenAnswer;
 	private float score;
 	
+	private TestTaken testTaken;
+	private ClosedQuiz quiz;
+	
 	// Methods
 	public char getGivenAnswer() { return givenAnswer; }
 	public void setGivenAnswer(char givenAnswer) { this.givenAnswer = givenAnswer; }
 	
 	public float getScore() { return score; }
 	public void setScore(float score) { this.score = score; }
+	
+	
+	public TestTaken getTestTaken() { return testTaken;	}
+	public void setTestTaken(TestTaken testTaken) {	this.testTaken = testTaken;	}
+	
+	public ClosedQuiz getQuiz() { return quiz; }
+	public void setQuiz(ClosedQuiz quiz) { this.quiz = quiz; }
 }
--- Object orientation/Codice sorgente/src/Model/OpenAnswer.java
+++ Object orientation/Codice sorgente/src/Model/OpenAnswer.java
@@ -5,10 +5,20 @@ public class OpenAnswer {
 	private String givenAnswer;
 	private float score;
 	
+	private TestTaken testTaken;
+	private OpenQuiz quiz;
+	
 	// Methods
 	public String getGivenAnswer() { return givenAnswer; }
 	public void setGivenAnswer(String givenAnswer) { this.givenAnswer = givenAnswer; }
 	
 	public float getScore() { return score; }
 	public void setScore(float score) { this.score = score; }
+	
+	
+	public TestTaken getTestTaken() { return testTaken;	}
+	public void setTestTaken(TestTaken testTaken) {	this.testTaken = testTaken;	}
+	
+	public OpenQuiz getQuiz() {	return quiz; }
+	public void setQuiz(OpenQuiz quiz) { this.quiz = quiz; }
 }
