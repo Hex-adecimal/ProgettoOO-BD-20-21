ffdeb1f93f7a82164fa2ad7569f7c7911bef5607
MoodSwing159 <93043279+MoodSwing159@users.noreply.github.com>
2022-02-21 15:35:34 +0100

    Add files via upload
    CHANGES:
    - Added attributes "classes", "lectures", and "createdTests", with getters and setters, to implement relationships with other classes in the Model

--- Object orientation/Codice sorgente/src/Model/Professor.java
+++ Object orientation/Codice sorgente/src/Model/Professor.java
@@ -2,10 +2,15 @@ package Model;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class Professor extends User{
 	// Attributes
 	private String codP;
+	
+	private ArrayList<Class> classes;
+	private ArrayList<Lecture> lectures;
+	private ArrayList<Test> createdTests;
 	
 	// Methods
 	public Professor(String codP, String firstName, String lastName, String email, String username, String password)
@@ -22,11 +27,16 @@ public class Professor extends User{
 		this.setCodP(rs.getString("codp"));
 	}
 
-	public String getCodP() {
-		return codP;
-	}
+	public String getCodP() { return codP; }
+	public void setCodP(String codP) { this.codP = codP; }
 
-	public void setCodP(String codP) {
-		this.codP = codP;
-	}
+	
+	public ArrayList<Class> getClasses() { return classes; }
+	public void setClasses(ArrayList<Class> classes) { this.classes = classes; }
+
+	public ArrayList<Lecture> getLectures() { return lectures; }
+	public void setLectures(ArrayList<Lecture> lectures) { this.lectures = lectures; }
+
+	public ArrayList<Test> getCreatedTests() { return createdTests; }
+	public void setCreatedTests(ArrayList<Test> createdTests) { this.createdTests = createdTests; }
 }
