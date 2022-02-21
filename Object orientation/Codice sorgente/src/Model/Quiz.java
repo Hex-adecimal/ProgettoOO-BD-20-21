576ae78530ecd2326e0b49f91613b28c7376aa0e
MoodSwing159 <93043279+MoodSwing159@users.noreply.github.com>
2022-02-21 15:37:24 +0100

    Add files via upload
    CHANGES:
    - Added attribute "test", with getter and setter, to implement relationships with other classes in the Model

--- Object orientation/Codice sorgente/src/Model/Quiz.java
+++ Object orientation/Codice sorgente/src/Model/Quiz.java
@@ -4,6 +4,13 @@ public class Quiz {
 	// Attributes
 	protected String question;
 	
-	// Methods
+	protected Test test;
 	
+	// Methods
+	public String getQuestion() { return question; }
+	public void setQuestion(String question) { this.question = question; }
+	
+	
+	public Test getTest() { return test; }
+	public void setTest(Test test) { this.test = test; }
 }
